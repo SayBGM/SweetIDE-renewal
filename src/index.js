f35d39b1c67eabc7ed10cc9c85283b0c6e11b7e2
seoyunho <yunho101775@gmail.com>
2018-05-19 11:54:58 +0900

    [CREATE] react-router v4 적용

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SweetIDE from './container/SweetIDE/SweetIDE';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import {store} from './core/redux/store/createStore'
-import registerServiceWorker from './registerServiceWorker';
+import { store } from './core/redux/store/createStore';
 
 ReactDOM.render(
   <Provider store={store}>
-    <SweetIDE/>
+    <Router>
+      <Switch>
+        <Route path='/ide' component={SweetIDE} exact/>
+      </Switch>
+    </Router>
   </Provider>
   , document.getElementById('root')
 );
-registerServiceWorker();
