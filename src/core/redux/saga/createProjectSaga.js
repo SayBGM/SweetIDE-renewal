3a8e2b49f0185b3b2c6f8f90dfa865ba9f45da3a
seoyunho <yunho101775@gmail.com>
2018-07-28 02:04:53 +0900

    add createProjectSaga

---
+++ src/core/redux/saga/createProjectSaga.js
@@ -0,0 +1,29 @@
+import { getCookie } from '../../utils/cookie';
+import { put, call } from 'redux-saga/effects';
+
+import { 
+  CREATE_PROJECT_SUCCESS,
+  CREATE_PROJECT_FAILURE
+} from '../actions/actionTypes';
+
+export function* createProject(getState) {
+  const response = yield call('/account', {
+    method: 'UPDATE',
+    headers: {
+      token: getCookie('token')
+    },
+    body: {
+      
+    }
+  });
+
+  if(response.status === 201) {
+    yield put({
+      type: CREATE_PROJECT_SUCCESS
+    });
+  }
+
+  yield put({
+    type: CREATE_PROJECT_FAILURE
+  });
+}
--- src/core/redux/saga/index.js
+++ src/core/redux/saga/index.js
@@ -1,12 +1,14 @@
 import { takeEvery } from 'redux-saga/effects';
 import { fetchProject, fetchDigitalMoudules, fetchAnalogMoudules } from './fetchDataSaga';
 import { modifyUserInfo } from './mypageSaga';
+import { createProject } from './createProjectSaga';
 
 import { 
   PERSONAL_PROJECT_LIST_REQUEST,
   MODIFY_USER_INFO_REQUEST,
   FETCH_DIGITAL_MODULES_REQUEST,
-  FETCH_ANALOG_MODULES_REQUEST
+  FETCH_ANALOG_MODULES_REQUEST,
+  CREATE_PROJECT_REQUEST
 } from '../actions/actionTypes';
 
 export default function* rootSaga(getState) {
@@ -14,6 +16,7 @@ export default function* rootSaga(getState) {
     yield takeEvery(PERSONAL_PROJECT_LIST_REQUEST, fetchProject),
     yield takeEvery(FETCH_DIGITAL_MODULES_REQUEST, fetchDigitalMoudules),
     yield takeEvery(FETCH_ANALOG_MODULES_REQUEST, fetchAnalogMoudules),
-    yield takeEvery(MODIFY_USER_INFO_REQUEST, modifyUserInfo, getState)
+    yield takeEvery(MODIFY_USER_INFO_REQUEST, modifyUserInfo, getState),
+    yield takeEvery(CREATE_PROJECT_REQUEST, createProject, getState)
   ];
 }
