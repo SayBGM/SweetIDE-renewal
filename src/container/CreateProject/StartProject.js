48a11d32704feb55c45020d17f7b606734f69d1f
GwangMinBack <bgm8862@gmail.com>
2018-07-29 22:26:10 +0900

    초기 포트 리스트 불러오기

--- src/container/CreateProject/StartProject.js
+++ src/container/CreateProject/StartProject.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import {connect} from 'react-redux';
 import './css/StartProject.scss';
+import {getPortList} from '../../core/redux/actions/projectAction';
+import ToastUtils from '../../core/utils/toaster/ToastUtils';
 
 class StartProject extends React.Component{
   render(){
@@ -12,12 +15,33 @@ class StartProject extends React.Component{
         <div>
           아마 석-민이 만든 애니메이션 들어갈 자리
         </div>
-        
-        <div className="SweetClient-start"><a href="sweetfab://">실행하기</a></div>
+        <div className="SweetClient-start" onClick={()=> this.inputList()}><a href="sweetfab://">실행하기</a></div>
         </div>
       </div>
     )
   }
+  inputList() {
+    axios.get('http://localhost:1601/').then(res => {
+      if(res.data === 'SweetFab'){
+        axios.get('http://localhost:1601/local/getportlist/').then(res => {
+          this.props.getPortList(res.data.com);
+          console.log(res.data.com);
+          return;
+        })
+      }
+      else {
+        ToastUtils.showErrorToast('SweetClient가 꺼져있습니다.<br/> 다시 한번 <strong>\'실행하기\'</strong>버튼을 눌러주세요.');
+      }
+    }).catch(err => {
+      ToastUtils.showErrorToast('SweetClient가 꺼져있습니다.<br/> 다시 한번 <strong>\'실행하기\'</strong>버튼을 눌러주세요.');
+    })
+  }
 }
 
-export default StartProject
+function mapDispatchtoProps(dispatch){
+  return {
+    getPortList: (list) => dispatch(getPortList(list)),
+  }
+}
+
+export default connect(null, mapDispatchtoProps)(StartProject)
